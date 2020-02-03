fadcdb868c69257712d1c31c2f63a74b59d2d45b
gregwhitaker <gwhitake@gmail.com>
2020-02-02 16:27:22 -0800

    Doing work

--- number-service/src/main/java/example/number/service/NumberService.java
+++ number-service/src/main/java/example/number/service/NumberService.java
@@ -1,8 +1,48 @@
 package example.number.service;
 
+import io.rsocket.AbstractRSocket;
+import io.rsocket.ConnectionSetupPayload;
+import io.rsocket.Payload;
+import io.rsocket.RSocket;
+import io.rsocket.RSocketFactory;
+import io.rsocket.SocketAcceptor;
+import io.rsocket.frame.decoder.PayloadDecoder;
+import io.rsocket.transport.netty.server.TcpServerTransport;
+import io.rsocket.util.DefaultPayload;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.math.BigInteger;
+
 public class NumberService {
+    private static final Logger LOG = LoggerFactory.getLogger(NumberService.class);
 
-    public static void main(String... args) {
+    public static void main(String... args) throws InterruptedException {
+        RSocketFactory.receive()
+                .frameDecoder(PayloadDecoder.DEFAULT)
+                .acceptor(new SocketAcceptor() {
+                    @Override
+                    public Mono<RSocket> accept(ConnectionSetupPayload setup, RSocket sendingSocket) {
+                        return Mono.just(new AbstractRSocket() {
+                            @Override
+                            public Flux<Payload> requestStream(Payload payload) {
+                                return Flux.range(1, Integer.MAX_VALUE)
+                                        .map(i -> {
+                                            LOG.info("Sending: {}", i);
+                                            return DefaultPayload.create(BigInteger.valueOf(i).toByteArray());
+                                        });
+                            }
+                        });
+                    }
+                })
+                .transport(TcpServerTransport.create(7002))
+                .start()
+                .block();
 
+        LOG.info("RSocket server started on port: 7002");
+
+        Thread.currentThread().join();
     }
 }
